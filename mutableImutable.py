82462400c7e507ff352b8b19a9f1f161f5755e8e
ten8cousb <ten8ciousb@gmail.com>
2021-04-29 17:35:54 -0500

    added type

--- mutableImutable.py
+++ mutableImutable.py
@@ -1,62 +1,62 @@
 print("int")
 a=1
 b=a
-print(f"initial the value of a is {a}")
-print(f"initial value of b is {b}")
+print(f"initial the value of {type(a)} a is {a} ")
+print(f"initial value of {type(b)} a is {b} ")
 a=5
-print(f"after change the value of a is {a}")
-print(f"after change the value of b is {b}")
+print(f"after change the value of {type(a)} a is {a} ")
+print(f"after change the value of {type(b)} a is {b} ")
 
 
 print("Float")
 a=42.0
 b=a
-print(f"initial the value of a is {a}")
-print(f"initial value of b is {b}")
+print(f"initial the value of {type(a)} a is {a} ")
+print(f"initial value of {type(b)} a is {b} ")
 
 a=3.14
-print(f"after change the value of a is {a}")
-print(f"after change the value of b is {b}")
+print(f"after change the value of {type(a)} a is {a} ")
+print(f"after change the value of {type(b)} a is {b} ")
 
 
 print("Strings")
 a="That's what I said"
 b=a
-print(f"initial the value of a is {a}")
-print(f"initial value of b is {b}")
+print(f"initial the value of {type(a)} a is {a} ")
+print(f"initial value of {type(b)} a is {b} ")
 
 a="Who's on First"
-print(f"after change the value of a is {a}")
-print(f"after change the value of b is {b}")
+print(f"after change the value of {type(a)} a is {a} ")
+print(f"after change the value of {type(b)} a is {b} ")
 
 print ("Lists")
 a= [42, 3.14, 9]
 b= a
-print(f"initial the value of a is {a}")
-print(f"initial value of b is {b}")
+print(f"initial the value of {type(a)} a is {a} ")
+print(f"initial value of {type(b)} a is {b} ")
 
 a[2] = "now what"
-print(f"after change the value of a is {a}")
-print(f"after change the value of b is {b}")
+print(f"after change the value of {type(a)} a is {a} ")
+print(f"after change the value of {type(b)} a is {b} ")
 
 
 print ("Tuples")
 a= (42, 3.14, 9)
 b= a
-print(f"initial the value of a is {a}")
-print(f"initial value of b is {b}")
+print(f"initial the value of {type(a)} a is {a} ")
+print(f"initial value of {type(b)} a is {b} ")
 
 a= (42, 3.14, "that's what")
-print(f"after change the value of a is {a}")
-print(f"after change the value of b is {b}")
+print(f"after change the value of {type(a)} a is {a} ")
+print(f"after change the value of {type(b)} a is {b} ")
 
 
 print ("Dictionary")
 a= {"answer":42, "pi":3.14, "number":9}
 b= a
-print(f"initial the value of a is {a}")
-print(f"initial value of b is {b}")
+print(f"initial the value of {type(a)} a is {a} ")
+print(f"initial value of {type(b)} a is {b} ")
 
 a["question"] ="?"
-print(f"after change the value of a is {a}")
-print(f"after change the value of b is {b}")
+print(f"after change the value of {type(a)} a is {a} ")
+print(f"after change the value of {type(b)} a is {b} ")
