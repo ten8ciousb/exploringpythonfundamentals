214fb006765064377b64988c0714152b5aca72f0
ten8cousb <ten8ciousb@gmail.com>
2021-04-29 23:36:31 -0500

    added summary output

--- containersOrVariables.py
+++ containersOrVariables.py
@@ -61,9 +61,6 @@ print(f"after change the value of {type(a)} a is {a} with id of {id(a)} ")
 print(f"after change the value of {type(b)} b is {b} with id of {id(b)} ")
 print(f"Assigning new list to a {variabletype} changes id? {id(a) != id(b)}")
 
-
-
-
 print()
 variabletype="Tuple"
 print (variabletype)
@@ -92,8 +89,22 @@ print(f"after change the value of {type(a)} a is {a} with id of {id(a)} ")
 print(f"after change the value of {type(b)} b is {b} with id of {id(b)} ")
 print(f"Adding an element to a {variabletype} changes id? {id(a) != id(b)}")
 print()
-print("changing element in {variabletype}")
+print(f"changing element in {variabletype}")
 a["question"] ="What do you think"
 print(f"after change the value of {type(a)} a is {a} with id of {id(a)} ")
 print(f"after change the value of {type(b)} b is {b} with id of {id(b)} ")
 print(f"Changing an element in a {variabletype} changes id? {id(a) != id(b)}")
+print()
+print("summary")
+print ("__________")
+print("Changing a int changes id? True")
+print("Changing a Float changes id? True")
+print("Changing a String changes id? True")
+print("Assigning new list to a List changes id? True")
+print("Changing an element in a Tuple changes id? True")
+print("variables for int, float, string, tuple are containers. Variables refer to different objects")
+print()
+print("changing an element in a List changes id? False")
+print("Adding an element to a Dictionary changes id? False")
+print("Changing an element in a Dictionary changes id? False")
+print("variables for Lists and Dictonaries act like labels. Variables refer to the same objects")
